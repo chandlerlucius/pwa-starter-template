3cd680c261abf6db6f026f6ba42115a1076b9466
Chandler Lucius <clucius@chandlerlucius.com>
2020-05-14 19:46:23 -0500

    Added service worker

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+
+const CACHE = "pwa-starter-template-cache";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+const networkFirstPaths = [
+    "**.html"
+];
+
+networkFirstPaths.forEach((path) => {
+  workbox.routing.registerRoute(
+    new RegExp(path),
+    new workbox.strategies.NetworkFirst({
+      cacheName: CACHE
+    })
+  );
+});
