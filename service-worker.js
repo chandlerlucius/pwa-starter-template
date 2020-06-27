4b78d4b82e95ee75028a04544d0948715511fc55
Chandler Lucius <clucius@chandlerlucius.com>
2020-06-26 23:43:14 -0500

    Added share target testing

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,17 @@ workbox.routing.registerRoute(
   new RegExp('/*'),
   new workbox.strategies.NetworkFirst()
 );
+
+const shareTargetHandler = async ({event}) => {
+  const formData = await event.request.formData();
+  const mediaFiles = formData.getAll('media');
+  for (const mediaFile of mediaFiles) {
+    console.log(mediaFile.name + " " + mediaFile.size + " " + mediaFile.type);
+  }
+};
+
+workbox.registerRoute(
+  '/_share-target',
+  shareTargetHandler,
+  'POST'
+);
