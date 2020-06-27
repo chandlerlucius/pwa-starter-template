ddaf79ef68c215a157ab7f62770c4ed92072507f
Chandler Lucius <clucius@chandlerlucius.com>
2020-06-27 01:12:52 -0500

    Updates to web share testing

--- service-worker.js
+++ service-worker.js
@@ -7,15 +7,24 @@ workbox.routing.registerRoute(
   new workbox.strategies.NetworkFirst()
 );
 
-const shareTargetHandler = async ({
-  event
-}) => {
-  const client = await self.clients.get(event.resultingClientId || event.clientId);
-  const formData = await event.request.formData();
-  const mediaFiles = formData.getAll('media');
-  for (const mediaFile of mediaFiles) {
-    client.postMessage({mediaFile, action: 'load-image'});
-  }
+onfetch = async (event) => {
+  if (event.request.method !== 'POST') return;
+
+  /* This is to fix the issue Jake found */
+  event.respondWith(Response.redirect('/share/image/'));
+
+  event.waitUntil(async function () {
+    const data = await event.request.formData();
+    const client = await self.clients.get(event.resultingClientId || event.clientId);
+    // Get the data from the named element 'file'
+    const file = data.get('file');
+
+    console.log('file', file);
+    client.postMessage({
+      file,
+      action: 'load-image'
+    });
+  }());
 };
 
 workbox.routing.registerRoute(
