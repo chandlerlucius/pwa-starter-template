bcf3c736d9adccd7cc0ed4113664aa5a52bdd628
Chandler Lucius <clucius@chandlerlucius.com>
2020-06-28 12:24:09 -0500

    Updates for Web Share API Testing

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,10 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 self.addEventListener("fetch", function (event) {
-  if (event.request.method === 'POST' && event.request.url.indexOf('/share/image') > -1) {
+  if (event.request.method === 'POST') {
     event.respondWith((async () => {
       // const data = await event.request.formData();
+      self.clients.claim();
       const client = await self.clients.get(event.resultingClientId || event.clientId);
       // const files = data.getAll('files');
 
